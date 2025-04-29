de02820c7e9c3ad780143aea457fc8c6ef7954cd
tavice <thomasavice.ta@gmail.com>
2025-04-29 10:27:03 -0700

    updated json

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // This file is just a wrapper to import the main application
-require('./src/index.js'); 
+require('dotenv').config();
+const app = require('./src/index.js');
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Allowed origins: ${process.env.ALLOWED_ORIGINS}`);
+}); 
